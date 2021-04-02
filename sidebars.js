5ee1cf3c61b6acb2b4a78b51c9010ca565e1f856
Tristan Dyer <atdyer@ncsu.edu>
2021-04-02 17:30:47 -0400

    Docusaurus

---
+++ sidebars.js
@@ -0,0 +1,37 @@
+module.exports = {
+    docs: [
+        {
+            type: 'category',
+            label: 'Introduction',
+            items: [
+                'introduction/getting-started',
+                'introduction/installation',
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Working with Graphs',
+            items: [
+                'working-with-graphs/introduction'
+            ]
+        },
+        {
+            type: 'category',
+            label: 'Visualizing Graphs',
+            items: [
+                'visualizing-graphs/introduction'
+            ]
+        },
+        {
+            type: 'category',
+            label: 'Docusaurus',
+            items: [
+                'create-a-page',
+                'create-a-document',
+                'create-a-blog-post',
+                'markdown-features',
+                'thank-you',
+            ]
+        }
+    ],
+};
---
+++ src/pages/test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+function Hello () {
+    return <Layout>
+        <h1>Hello</h1>
+    </Layout>
+}
+
+export default Hello;
